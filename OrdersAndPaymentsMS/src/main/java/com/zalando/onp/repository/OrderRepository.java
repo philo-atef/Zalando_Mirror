5a39f9351978553f6299f83b079771e203add3a9
PhiloAmgad <86148343+PhiloAmgad@users.noreply.github.com>
2023-05-11 15:41:29 +0300

    Revert "main"

--- OrdersAndPaymentsMS/src/main/java/com/zalando/onp/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zalando.onp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.zalando.onp.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- OrdersAndPaymentsMS/src/main/java/com/zalando/onp/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zalando.onp.repository;
-
-import com.zalando.onp.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
-
-}
