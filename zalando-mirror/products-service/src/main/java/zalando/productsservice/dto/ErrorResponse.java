3131634af84f2cfd95719621703f8f5902ff5d84
Mohamed Rostom <mohamedrostom62@gmail.com>
2023-05-19 16:17:06 +0300

    add mongo validations

---
+++ zalando-mirror/products-service/src/main/java/zalando/productsservice/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package zalando.productsservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+    private String stackTrace;
+}
