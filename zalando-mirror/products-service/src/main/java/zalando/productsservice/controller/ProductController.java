060bab897ae3d2560f58349c038f2b1be9f4b07c
Mohamed Rostom <62266362+MohamedRostom0@users.noreply.github.com>
2023-05-19 16:19:59 +0300

    products/add-mongo-validations-and-rabbitmq-setup (#6)
    * add mongo validations
    
    * add rabbitmq config and producer

--- zalando-mirror/products-service/src/main/java/zalando/productsservice/controller/ProductController.java
+++ zalando-mirror/products-service/src/main/java/zalando/productsservice/controller/ProductController.java
@@ -1,12 +1,21 @@
 package zalando.productsservice.controller;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+import zalando.productsservice.dto.ErrorResponse;
 import zalando.productsservice.dto.ProductRequest;
+import zalando.productsservice.exception.ProductNotFoundException;
 import zalando.productsservice.model.Product;
+import zalando.productsservice.rabbitmq.RabbitMQProducer;
 import zalando.productsservice.service.ProductService;
 ;import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/products")
@@ -15,33 +24,113 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Autowired
+    private RabbitMQProducer producer;
+
+
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest){
-        productService.createProduct(productRequest);
+    public ResponseEntity<Product> createProduct(@RequestBody ProductRequest productRequest){
+        Product product = productService.createProduct(productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
+
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Product> getProducts(){
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Product getProductById(@PathVariable String id){
-        return productService.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable String id){
+        Product product = productService.getProductById(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Product updateProduct(@PathVariable String id, @RequestBody ProductRequest productRequest){
-        return productService.updateProduct(id, productRequest);
+    public ResponseEntity<Product> updateProduct(@PathVariable String id, @RequestBody ProductRequest productRequest){
+        Product product = productService.updateProduct(id, productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteProduct(@PathVariable String id){
+    public ResponseEntity<Void> deleteProduct(@PathVariable String id){
         productService.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // Exceptions handling
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleItemNotFoundException(
+            ProductNotFoundException exception,
+            WebRequest request
+    ){
+        return buildErrorResponse(exception, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+            IllegalArgumentException exception,
+            WebRequest request
+    ){
+        return buildErrorResponse(exception, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(
+            ConstraintViolationException exception,
+            WebRequest request
+    ){
+        List<String> errors = exception.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toList());
+
+        return buildErrorResponse(exception, HttpStatus.UNPROCESSABLE_ENTITY, request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleAllUncaughtException(
+            Exception exception,
+            WebRequest request){
+        return buildErrorResponse(
+                exception,
+                "Unknown error occurred",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                request
+        );
+    }
+
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(
+            Exception exception,
+            HttpStatus httpStatus,
+            WebRequest request
+    ) {
+        return buildErrorResponse(
+                exception,
+                exception.getMessage(),
+                httpStatus,
+                request);
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(
+            Exception exception,
+            String message,
+            HttpStatus httpStatus,
+            WebRequest request
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                httpStatus.value(),
+                exception.getMessage()
+        );
+
+        return ResponseEntity.status(httpStatus).body(errorResponse);
     }
 }
--- zalando-mirror/products-service/src/main/java/zalando/productsservice/model/Product.java
+++ zalando-mirror/products-service/src/main/java/zalando/productsservice/model/Product.java
@@ -1,5 +1,7 @@
 package zalando.productsservice.model;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,7 @@ import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Document(collection = "product")
@@ -22,14 +25,91 @@ import java.util.List;
 public class Product {
     @Id
     private String id;
+    @NotNull
     private String brandId;
+    @NotNull
     private String brandName;
+    @NotNull
     private String name;
+    @NotNull
     private Double price;
+    @NotNull
     private String material;
+    @NotNull
     private List<String> colors;
+    @NotNull
     private List<String> sizes;
+    @NotNull
     private String gender;
+    @NotNull
     private String category;
+    @NotNull
     private String subcategory;
+
+
+    // Validation rules
+    @AssertTrue(message = "Invalid Gender: Gender can only be [Men's - Women's - Unisex]")
+    private boolean isValidGender(){
+        return Arrays.asList("Men's", "Women's", "Unisex").contains(gender);
+    }
+
+    @AssertTrue(message = "Invalid subcategory for the given category")
+    private boolean isValidSubcategory() {
+        List<String> VALID_CLOTHING_SUBCATEGORIES = Arrays.asList(
+                "T-shirts & Polos",
+                "Shirts",
+                "Sweatshirts & Hoodies",
+                "Trousers",
+                "Jeans",
+                "Shorts",
+                "Jackets",
+                "Knitwear",
+                "Sportswear",
+                "Tracksuits & Joggers",
+                "Suits & Tailoring",
+                "Coats",
+                "Underwear & Socks",
+                "Swimwear",
+                "Loungewear & Sleepwear"
+        );
+        List<String> VALID_SHOES_SUBCATEGORIES = Arrays.asList(
+                "Sneakers",
+                "Open shoes",
+                "Lace-up shoes",
+                "Loafers",
+                "Business shoes",
+                "Boots",
+                "Sports shoes",
+                "Outdoor shoes",
+                "Slippers",
+                "Shoe accessories"
+        );
+        List<String> VALID_ACCESSORIES_SUBCATEGORIES = Arrays.asList(
+                "Accessories",
+                "Bags & cases",
+                "Beanies, hats & caps",
+                "Sunglasses",
+                "Jewellery",
+                "Watches",
+                "Belts",
+                "Wallets & card holders",
+                "Blue-light glasses",
+                "Ties & accessories",
+                "Scarves & Shawls",
+                "Gloves",
+                "Lifestyle Electronics",
+                "Miscellaneous",
+                "Umbrellas",
+                "Masks"
+        );
+
+        if (category.equalsIgnoreCase("Clothing")) {
+            return VALID_CLOTHING_SUBCATEGORIES.contains(subcategory);
+        } else if (category.equalsIgnoreCase("Shoes")) {
+            return VALID_SHOES_SUBCATEGORIES.contains(subcategory);
+        } else if (category.equalsIgnoreCase("Accessories")) {
+            return VALID_ACCESSORIES_SUBCATEGORIES.contains(subcategory);
+        }
+        return false;
+    }
 }
