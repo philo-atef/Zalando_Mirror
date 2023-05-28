22e874b99a49abc5055ebadc4f6b49499417a8d8
Mark Francis <markfrancis135@gmail.com>
2023-05-28 18:50:04 +0300

    Eureka Configuration In Authentication App

--- zalando-mirror/users-service/src/main/java/com/example/demo/AuthenticationApplication.java
+++ zalando-mirror/users-service/src/main/java/com/example/demo/AuthenticationApplication.java
@@ -6,10 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @RestController
 @SpringBootApplication
+
+
 @RequestMapping("/api/v1/demo-controller")
+@EnableDiscoveryClient
 public class AuthenticationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
