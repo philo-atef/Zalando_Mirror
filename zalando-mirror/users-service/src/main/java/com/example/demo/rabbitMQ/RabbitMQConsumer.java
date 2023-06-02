e734e96ea1560e7740709b33dea7524de0f70904
philo-atef <philopateer.badeer@gmail.com>
2023-06-02 23:28:43 +0300

    added docker compose

--- zalando-mirror/users-service/src/main/java/com/example/demo/rabbitMQ/RabbitMQConsumer.java
+++ zalando-mirror/users-service/src/main/java/com/example/demo/rabbitMQ/RabbitMQConsumer.java
@@ -4,7 +4,6 @@ import com.example.demo.customer.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +12,11 @@ public class RabbitMQConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
     private final CustomerService customerService;
 
-    @RabbitListener(queues = "userDetailsQueue")
-    public Object getCustomerDetails(String userID){
-        LOGGER.info(String.format("Received User ID -> %s", userID));
-        Object result = customerService.getCustomerDetailsByID(Long.parseLong(userID));
-        LOGGER.info(String.format("Received User Details -> %s", result.toString()));
-        return (result);
-    }
+//    @RabbitListener(queues = "userDetailsQueue")
+//    public Object getCustomerDetails(String userID){
+//        LOGGER.info(String.format("Received User ID -> %s", userID));
+//        Object result = customerService.getCustomerDetailsByID(Long.parseLong(userID));
+//        LOGGER.info(String.format("Received User Details -> %s", result.toString()));
+//        return (result);
+//    }
 }
